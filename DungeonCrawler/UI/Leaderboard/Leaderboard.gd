5ee4719a7e241a35aad3436512956e9de082fa4e
TanJunHong <b190013@e.ntu.edu.sg>
2021-10-10 09:38:47 +0800

    Add leaderboard to leaderboard page

--- DungeonCrawler/UI/Leaderboard/Leaderboard.gd
+++ DungeonCrawler/UI/Leaderboard/Leaderboard.gd
@@ -1,9 +1,18 @@
 extends Control
 
 var scene_path_to_load 
+onready var column : VBoxContainer = $Leaderboard
+export var score_row : PackedScene = preload("res://DungeonCrawler/UI/Leaderboard/ScoreRow.tscn")
+export var title_row : PackedScene = preload("res://DungeonCrawler/UI/Leaderboard/TitleRow.tscn")
 
 func _ready():
 	$BackButton.connect("pressed", self, "_on_Button_pressed", [$BackButton.scene_to_load])	
+	
+	var new_title_row := title_row.instance() as TitleRow
+	column.add_child(new_title_row)
+	new_title_row.username.text = "Name"
+	new_title_row.score.text = "Score"
+	
 	var query : FirestoreQuery = FirestoreQuery.new()
 	query.select(["username", "scores.level1"])
 	query.from("users")
@@ -11,7 +20,8 @@ func _ready():
 	query.limit(10)
 	var query_task : FirestoreTask = Firebase.Firestore.query(query)
 	var result = yield(query_task, "task_finished")
-	print(result)
+	delete_current_rows()
+	add_new_rows(result)
 
 func _on_BackButton_pressed():
 	$FadeIn.show()
@@ -25,6 +35,14 @@ func _on_Button_pressed(scene_to_load):
 func _on_FadeIn_fade_finished():
 	get_tree().change_scene(scene_path_to_load)
 
+func delete_current_rows() -> void:
+	for child in column.get_children():
+		if child is ScoreRow:
+			child.queue_free()
 
-func add_new_rows():
-	pass
+func add_new_rows(users: Array) -> void:
+	for user in users:
+		var new_score_row := score_row.instance() as ScoreRow
+		column.add_child(new_score_row)
+		new_score_row.username.text = user.doc_fields.username
+		new_score_row.score.text = str(user.doc_fields.scores.level1)
---
+++ DungeonCrawler/UI/Leaderboard/ScoreRow.gd
@@ -0,0 +1,11 @@
+extends HBoxContainer
+class_name ScoreRow
+
+onready var username : Label = $Username
+onready var score : Label = $Score
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
