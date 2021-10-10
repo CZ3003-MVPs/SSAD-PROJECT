2a354fe8a942fe7e5dc03c0b197e61d29fd406d4
TanJunHong <b190013@e.ntu.edu.sg>
2021-10-11 01:00:05 +0800

    Toggle between different level leaderboards

--- DungeonCrawler/UI/Leaderboard/Leaderboard.gd
+++ DungeonCrawler/UI/Leaderboard/Leaderboard.gd
@@ -2,6 +2,7 @@ extends Control
 
 var scene_path_to_load 
 onready var column : VBoxContainer = $Leaderboard
+onready var dropdown : OptionButton = $LevelContainer/LevelSelector
 export var score_row : PackedScene = preload("res://DungeonCrawler/UI/Leaderboard/ScoreRow.tscn")
 export var title_row : PackedScene = preload("res://DungeonCrawler/UI/Leaderboard/TitleRow.tscn")
 
@@ -12,6 +13,11 @@ func _ready():
 	column.add_child(new_title_row)
 	new_title_row.username.text = "Name"
 	new_title_row.score.text = "Score"
+	
+	dropdown.add_item("All")
+	dropdown.add_separator()
+	dropdown.add_item("1")
+	dropdown.add_item("2")
 	
 	var query : FirestoreQuery = FirestoreQuery.new()
 	query.select(["username", "scores.level1"])
@@ -47,4 +53,22 @@ func add_new_rows(users: Array) -> void:
 		var new_score_row := score_row.instance() as ScoreRow
 		column.add_child(new_score_row)
 		new_score_row.username.text = user.doc_fields.username
-		new_score_row.score.text = str(user.doc_fields.scores.level1)
+		new_score_row.score.text = str(user.doc_fields.scores.values()[0])
+
+func get_leaderboard(level):
+	if level == "All":
+		return
+	var score_field = "scores.level" + level
+	var query : FirestoreQuery = FirestoreQuery.new()
+	query.select(["username", score_field])
+	query.from("users")
+	query.order_by(score_field, FirestoreQuery.DIRECTION.DESCENDING)
+	query.limit(10)
+	var query_task : FirestoreTask = Firebase.Firestore.query(query)
+	var result = yield(query_task, "task_finished")
+	delete_current_rows()
+	print(result)
+	add_new_rows(result)
+
+func _on_LevelSelector_item_selected(index):
+	get_leaderboard(dropdown.get_item_text(index))
