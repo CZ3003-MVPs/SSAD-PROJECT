d05e940cdbacd7fccc0d5d721626c9dfc0ce7dc1
TanJunHong <b190013@e.ntu.edu.sg>
2021-10-11 11:07:41 +0800

    Add documentations to login and register page

--- DungeonCrawler/UI/Login/Login.gd
+++ DungeonCrawler/UI/Login/Login.gd
@@ -6,22 +6,31 @@ onready var notification : Label = $Notification
 
 var scene_path_to_load
 
+# Loads on startup
+# Email will be on focus
 func _ready() -> void:
 	Firebase.Auth.connect("login_succeeded", self, "_on_FirebaseAuth_login_succeeded")
 	Firebase.Auth.connect("login_failed", self, "on_login_failed")
-	email.grab_focus() # adding keyboard support (set focus on the email line edit)
+	
+	# adding keyboard support (set focus on the email line edit)
+	email.grab_focus() 
 
+# Signal after pressing login button
 func _on_LoginButton_pressed() -> void:
 	if email.text.empty() or password.text.empty():
 		notification.text = "Enter your email and password!"
 		return
 	Firebase.Auth.login_with_email_and_password(email.text, password.text)
 
+# Signal after pressing register button
+# User will be redirected to register page
 func _on_RegisterButton_pressed() -> void:
 	scene_path_to_load = "res://DungeonCrawler/UI/Register/Register.tscn"
 	$FadeIn.show()
 	$FadeIn.fade_in() 
 
+# Signal after successful login
+# User will be redirected to main menu
 func _on_FirebaseAuth_login_succeeded(auth):
 	Backend.set_user_info(auth)
 	notification.text = "Login Successful!"
@@ -29,10 +38,13 @@ func _on_FirebaseAuth_login_succeeded(auth):
 	scene_path_to_load = "res://DungeonCrawler/UI/MenuPage/MainMenu.tscn"
 	$FadeIn.show()
 	$FadeIn.fade_in() 
-	
+
+# Signal after failed login
+# User will be shown the error
 func on_login_failed(error_code, message):
 	notification.text = message.capitalize()
 
-
+# Signal after fade finishes
+# User will be redirected accordingly
 func _on_FadeIn_fade_finished():
 	get_tree().change_scene(scene_path_to_load)
--- DungeonCrawler/UI/Register/Register.gd
+++ DungeonCrawler/UI/Register/Register.gd
@@ -7,18 +7,23 @@ onready var notification : Label = $Notification
 
 var scene_path_to_load
 
+# Loads on startup
+# Email will be on focus
 func _ready():
 	Firebase.Auth.connect("signup_succeeded", self, "_on_FirebaseAuth_login_succeeded")
 	Firebase.Auth.connect("signup_failed", self, "on_signup_failed")
 	email.grab_focus() # adding keyboard support (set focus on the email line edit)
 
+# Signal after pressing register button
 func _on_RegisterButton_pressed() -> void:
 	if password.text != confirm.text or email.text.empty() or password.text.empty():
 		notification.text = "Invalid email/password!"
 		return
-	#Firebase.register(email.text, password.text, http)
+
 	Firebase.Auth.signup_with_email_and_password(email.text, password.text)
 
+# Signal after successful registration
+# User will be redirected to login page
 func _on_FirebaseAuth_login_succeeded(auth):
 	notification.text = "Registration sucessful! Redirecting you to login page..."
 	yield(get_tree().create_timer(2.0), "timeout")
@@ -26,14 +31,19 @@ func _on_FirebaseAuth_login_succeeded(auth):
 	$FadeIn.show()
 	$FadeIn.fade_in() 
 
+# Signal after failed registration
+# User will show a notification text on failure
 func on_signup_failed(error_code, message):
 	notification.text = message.capitalize()
 
+# Signal after back button pressed
+# User will be redirected to login screen
 func _on_BackButton_pressed():
 	scene_path_to_load = "res://DungeonCrawler/UI/Login/Login.tscn"
 	$FadeIn.show()
 	$FadeIn.fade_in() 
 
-
+# Signal after fade finishes
+# User will be redirected accordingly
 func _on_FadeIn_fade_finished():
 	get_tree().change_scene(scene_path_to_load)
