5456f21aa284f1ca512ad79c69eba3a0bd545ff5
s-t-e-f <65395025+s-t-e-f@users.noreply.github.com>
2021-10-13 21:59:00 +0800

    Edited the change password page

--- DungeonCrawler/UI/Settings/ChangePassword.gd
+++ DungeonCrawler/UI/Settings/ChangePassword.gd
@@ -4,14 +4,20 @@ var scene_path_to_load
 # Loads on startup
 func _ready():
 	$BackButton.connect("pressed", self, "_on_Button_pressed", [$BackButton.scene_to_load])	
-
+	
+	$ChangePwConfirmation.visible = false
+	$SuccessNotification.visible = false
+	
+	# Change password confirmation dialog
+	$ChangePwConfirmation.get_ok().text = "Yes"
+	$ChangePwConfirmation.get_cancel().text = "No"
+	
 # Signal after back button pressed
 # User will be redirected to previous page
 func _on_BackButton_pressed():
 	scene_path_to_load = "res://DungeonCrawler/UI/Settings/Settings.tscn"
 	$FadeIn.show()
 	$FadeIn.fade_in() 
-
 
 # Signal after fade finishes
 # User will be redirected accordingly
@@ -20,4 +26,16 @@ func _on_FadeIn_fade_finished():
 
 
 func _on_SubmitButton_pressed():
-	pass # Replace with function body.
+	$ChangePwConfirmation.visible = true
+
+func _on_ChangePwConfirmation_confirmed():
+	# Backend codes here
+	
+	# Show email is sent notification 
+	$ChangePwConfirmation.visible = false
+	$SuccessNotification.visible = true
+	
+func _on_SuccessNotification_confirmed():
+	scene_path_to_load = "res://DungeonCrawler/UI/MenuPage/MainMenu.tscn"
+	$FadeIn.show()
+	$FadeIn.fade_in() 
