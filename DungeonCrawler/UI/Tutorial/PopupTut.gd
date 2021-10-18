a982e0d3b00b00999e1c3d1bb9f2abd2e54cb6fb
stefanie <stefanie.low@hotmail.com>
2021-10-18 21:30:19 +0800

    Included the stop button in tutorial page

--- DungeonCrawler/UI/Tutorial/PopupTut.gd
+++ DungeonCrawler/UI/Tutorial/PopupTut.gd
@@ -2,39 +2,55 @@ extends Control
 
 func _on_CloseButton_pressed():
 	$DragCodeBlocks.visible = false
-	$DeleteAndReset.visible = false
+	$ResetAndStop.visible = false
 	$RunCodeBlocks.visible = false
+	$DeleteCodeBlocks.visible = false
 	$LevelCompletion.visible = false
 
-# Moving to the DeleteAndReset tutorial page
+# Moving to the DeleteCodeBlocks tutorial page
 func _on_NextButton1_pressed():
 	$DragCodeBlocks.visible = false
-	$DeleteAndReset.visible = true
+	$DeleteCodeBlocks.visible = true
 
 # Moving to the Run Code Block tutorial page 
 func _on_NextButton2_pressed():
-	$DeleteAndReset.visible = false
+	$DeleteCodeBlocks.visible = false
 	$RunCodeBlocks.visible = true
 
-# Moving to the Level Completion tutorial page 
+# Moving to the ResetAndStop Completion tutorial page 
 func _on_NextButton3_pressed():
 	$RunCodeBlocks.visible = false
+	$ResetAndStop.visible = true
+	
+# Moving to LevelCompletion tutorial page 
+func _on_NextButton4_pressed():
+	$ResetAndStop.visible = false
 	$LevelCompletion.visible = true
 
 # Moving back to DragCodeBlock tutorial page
 func _on_PrevButton2_pressed():
-	$DeleteAndReset.visible = false
+	$DeleteCodeBlocks.visible = false
 	$DragCodeBlocks.visible = true
 	
-# Moving back to the DeleteAndReset Code Block tutorial page 
+# Moving back to the DeleteCodeBlocks Code Block tutorial page 
 func _on_PrevButton3_pressed():
 	$RunCodeBlocks.visible = false
-	$DeleteAndReset.visible = true
+	$DeleteCodeBlocks.visible = true
 
-# Moving back to the Run Code Block tutorial page 
+# Moving back to RunCodeBlocks tutorial page 
 func _on_PrevButton4_pressed():
+	$ResetAndStop.visible = false
+	$RunCodeBlocks.visible = true 
+
+# Moving back to the ResetAndStop Code Block tutorial page 
+func _on_PrevButton5_pressed():
 	$LevelCompletion.visible = false
-	$RunCodeBlocks.visible = true
+	$ResetAndStop.visible = true
+
+
+
+
+
 
 
 
