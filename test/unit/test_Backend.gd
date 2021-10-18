a27ee9783476857ad74855d237154721f2a5d73d
TanJunHong <b190013@e.ntu.edu.sg>
2021-10-18 23:01:08 +0800

    Update backend test

--- test/unit/test_Backend.gd
+++ test/unit/test_Backend.gd
@@ -8,8 +8,4 @@ func before_each():
 
 func after_each():
 	_backend.free()
-
-func test_set_user_info():
-	_backend.set_user_info({"idtoken": "12345",
-		"localid": "54321"})
-	assert_eq_deep(_backend.user_info, {"token": "12345", "id": "54321"})
+	
