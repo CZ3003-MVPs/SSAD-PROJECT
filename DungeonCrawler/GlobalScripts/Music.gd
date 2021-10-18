3a0c226f1d8f1d448810bb30c3923e9cc04c1905
sleepypanda <cheeyang85@gmail.com>
2021-10-18 12:36:39 +0800

    Made default sprite animation duration slightly faster by changing from 1.2 to 0.5. Added Music for main menu and level.

---
+++ DungeonCrawler/GlobalScripts/Music.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var currently_playing : String
+
+func play_sound(sound_to_play : String) -> void:
+	if !currently_playing.empty():
+		var node_of_currently_playing_music : AudioStreamPlayer = get_node(currently_playing)
+		node_of_currently_playing_music.stop()
+	var node_of_sound : AudioStreamPlayer = get_node(sound_to_play)
+	if node_of_sound != null:
+		currently_playing = sound_to_play
+		node_of_sound.play()
