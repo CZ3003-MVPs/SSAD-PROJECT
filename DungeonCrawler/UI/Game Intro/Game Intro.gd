c38e18b0ed2cf66525bb824de259ba13d16cc120
Keith Siow <keith97@live.com>
2021-10-21 00:34:06 +0800

    added in game intro
    trial with sample game intro music - WIP
    (mouse click)

---
+++ DungeonCrawler/UI/Game Intro/Game Intro.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	$AnimationPlayer.play("Fade in")
+	yield(get_tree().create_timer(1.5), "timeout")
+	#AudioManager.play_music('Intro')
+	$AnimationPlayer.play("Fade out")
+	yield(get_tree().create_timer(0.5),"timeout")
+	
+	get_tree().change_scene("res://DungeonCrawler/UI/Login/Login.tscn")
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
